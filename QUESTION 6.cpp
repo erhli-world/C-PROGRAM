56d071ff51bf293867d53d0a424252678cea787e
erhli-world <emmanuelanumerhli@hmail.com>
2026-06-15 11:32:35 +0000

    QUESTION 6

---
+++ QUESTION 6.cpp
@@ -0,0 +1,19 @@
+# A CODE THAT READS IN A CHARACTER AND CHECKS IF IT IS AN UPPERCASE LETTER
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    char ch;
+
+    cout << "Enter a character: ";
+    cin >> ch;
+
+    if (ch >= 'A' && ch <= 'Z') {
+        cout << "The character is an uppercase letter." << endl;
+    } else {
+        cout << "The character is not an uppercase letter." << endl;
+    }
+
+    return 0;
+}
