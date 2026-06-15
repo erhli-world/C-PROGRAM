bebf4a5a0d31e7bd8bdd329c4ca5d217ca06021c
erhli-world <emmanuelanumerhli@hmail.com>
2026-06-15 13:36:26 +0000

    QUESTION 1

--- QUESTION 1.cpp
+++ QUESTION 1.cpp
@@ -6,9 +6,9 @@ using namespace std;
 int main() {
     double num1,num2;
 
-cout <<"Enter the first number: ";
+cout <<" Enter the first number: ";
 cin >> num1;
-cout <<"Enter the second number: ";
+cout <<" Enter the second number: ";
 cin >> num2;
 
 cout << "Sum: " << num1 + num2 << endl;
@@ -16,9 +16,9 @@ cout << "Difference: " << num1 - num2 << endl;
 cout << "Product: " << num1 * num2 << endl;
 
 if(num2 != 0) {
-    cout << "Quotient: " << num1 / num2 << endl;    
+    cout << " Quotient: " << num1 / num2 << endl;    
 } else {
-    cout << "Quotient: Division by zero is not allowed." << endl;   
+    cout << " Quotient: Division by zero is not allowed." << endl;   
 }
 
     return 0;
