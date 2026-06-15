222fd80733e7136c4cd0f51e0bac4b9b5dbecb4e
erhli-world <emmanuelanumerhli@hmail.com>
2026-06-15 08:45:28 +0000

    QUESTION 2

--- QUESTION 1.cpp
+++ QUESTION 1.cpp
@@ -6,9 +6,9 @@ using namespace std;
 int main() {
     double num1,num2;
 
-cout <<"Enter the the first number: ";
+cout <<"Enter the first number: ";
 cin >> num1;
-cout <<"Enter the the second number: ";
+cout <<"Enter the second number: ";
 cin >> num2;
 
 cout << "Sum: " << num1 + num2 << endl;
---
+++ QUESTION 2.cpp
@@ -0,0 +1,18 @@
+//A CODE TO CALCULATE THE AREA OF A CIRCLE GIVEN THE RADIUS 
+
+#include <iostream>
+#include <cmath>
+using namespace std;        
+int main() {
+    double radius, area;
+    const double PI = 3.14159;
+
+    cout << "Enter the radius of the circle: ";
+    cin >> radius;
+
+    area = PI * pow(radius, 2);
+
+    cout << "The area of the circle is: " << area << endl;
+
+    return 0;
+}
