9defbd49f2cc19afd525a74dff67d9cd1fdb8e38
erhli-world <emmanuelanumerhli@hmail.com>
2026-06-15 14:29:13 +0000

    QUESTION 15

---
+++ QUESTION 15.cpp
@@ -0,0 +1,18 @@
+// A PROGRAM THAT USES CONDITIONAL OPERATOR TO DETERMINE THE LARGEST OF TWO FLOATING NUMBERS
+#include <iostream>
+using namespace std;
+
+int main(){
+    float num1,num2;
+    cout << " Enter first number: ";
+    cin>> num1;
+
+    cout<< " Enter second number: ";
+    cin>> num2;
+
+    float larger = (num1>num2)?num1:num2;
+
+    cout << " The larger number is: "<<larger<<endl;
+
+    return 0;
+}
