a3d8c7acc9601ce5bef0c72085c21310af68d938
erhli-world <emmanuelanumerhli@hmail.com>
2026-06-15 08:04:31 +0000

    QUESTION 1

--- QUESTION 1.cpp
+++ QUESTION 1.cpp
@@ -1,9 +1,10 @@
 // A PROGRAM THAT TAKES TWO NUMBERS AS INPUT AND PRINTS THEIR SUM,DIFFERENCE,PRODUCT AND QUOTIENT
+
 #include <iostream>
 using namespace std;
 
 int main() {
-    int num1,num2;
+    double num1,num2;
 
 cout <<"Enter the the first number: ";
 cin >> num1;
@@ -13,7 +14,12 @@ cin >> num2;
 cout << "Sum: " << num1 + num2 << endl;
 cout << "Difference: " << num1 - num2 << endl;
 cout << "Product: " << num1 * num2 << endl;
-cout << "Quotient: " << num1 / num2 << endl;
+
+if(num2 != 0) {
+    cout << "Quotient: " << num1 / num2 << endl;    
+} else {
+    cout << "Quotient: Division by zero is not allowed." << endl;   
+}
 
     return 0;
 }
