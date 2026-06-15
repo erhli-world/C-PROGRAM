5880a4e7fcad6e8bec71273a2b0f9887a69bab52
erhli-world <emmanuelanumerhli@hmail.com>
2026-06-15 11:11:19 +0000

    QUESTION 3

---
+++ QUESTION 3.cpp
@@ -0,0 +1,18 @@
+# A CODE THAT TAKES THREE INTEGER VALUES AS INPUT AND CHECKS IF THEY ARE EQUAL OR NOT
+
+#include <iostream>
+using namespace std;    
+int main() {
+    int num1, num2, num3;
+
+    cout << "Enter three integer values: ";
+    cin >> num1 >> num2 >> num3;
+
+    if (num1 == num2 && num2 == num3) {
+        cout << "EQUAL. " << endl;
+    } else {
+        cout << "NOT EQUAL. " << endl;
+    }
+
+    return 0;
+}
